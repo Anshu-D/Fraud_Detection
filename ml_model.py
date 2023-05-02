07c6b07fc0f2d734bf472cee77ca4a5c9f1593c7
Anshu-D <anshudhiman59@gmail.com>
2023-05-02 12:18:37 +0530

    adding the models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("adding all the ml related code here")
+
+
+def model_1():
+    print("adding code for ML model1")
+
+def model_2():
+    print("adding code for ML model2")
+
+def model_3():
+    print("adding code for ML model3")
